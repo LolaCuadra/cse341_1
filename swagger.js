82d57aecf6bb2f5e9623ea884c217eee7426a22f
LolaCuadra <lola_cuadra1940@byui.edu>
2024-02-03 23:29:19 -0700

    bug

--- swagger.js
+++ swagger.js
@@ -5,17 +5,11 @@ const doc = {
     title: 'My API',
     description: 'Temple API'
   },
-  host: 'cse341-instructor.herokuapp.com',
+  host: 'cse341-1-vpdx.onrender.com',
   schemes: ['https']
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-// generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
-
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./index.js');
-// });
