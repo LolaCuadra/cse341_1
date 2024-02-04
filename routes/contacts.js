9a0d5333adbac393602ae32b744bc8b76d69417e
LolaCuadra <lola_cuadra1940@byui.edu>
2024-02-03 23:20:45 -0700

    week04 error

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAll);
-router.post('/', contactsController.createContact);
+
 router.get('/:id', contactsController.getSingle);
+
+router.post('/', contactsController.createContact);
+
 router.put('/:id', contactsController.updateContact);
+
 router.delete('/:id', contactsController.deleteContact);
+
 module.exports = router;
